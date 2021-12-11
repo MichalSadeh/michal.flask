1c3a19f407de8569636de86f119f1f5741fc2c20
MichalSadeh <92794534+MichalSadeh@users.noreply.github.com>
2021-12-11 18:09:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from flask import Flask, redirect, url_for
 app = Flask(_name_)
 
 @app.route('/')
-def my_home():
+def my_home_page():
     return "welcome to my home page"
 
 @app.route('/homepage')
 def homepage():
-    return redirect(url_for('my_home'))
+    return redirect(url_for('my_home_page'))
 
 @app.route('/home')
 def home():
     return redirect('/')
 
 if _name_ == '_main_':
-    app.run()
+    app.run(debug=True)
